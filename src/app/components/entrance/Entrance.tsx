730b22187d02937202f941c8c8546db12b1f7689
naisaika <naisaikapoemelody@gmail.com>
2025-02-06 21:17:08 +0900

    making entrance button

--- src/app/components/entrance/Entrance.tsx
+++ src/app/components/entrance/Entrance.tsx
@@ -1,43 +1,44 @@
 "use client";
 import styles from "./Entrance.module.scss";
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState } from "react";
 import { Ajisai } from "./ajisai/Ajisai";
 import { Ripple } from "./ripple/Ripple";
 import { Pool } from "./pool/Pool";
 
 export const Entrance = () => {
   const [displayText, setDisplayText] = useState("");
-  const text = `毎 日お疲れさま。\nちょっと一息。\n寄り道しない・・・？`; // 改行を含むテキスト
-  const indexRef = useRef(0); // index を useRef で管理
+  const [index, setIndex] = useState(0);
+  const [showCursor, setShowCursor] = useState(false);
+  const text = `毎日お疲れさま。\nちょっと一息。\n寄り道しない・・・？`;
 
   useEffect(() => {
-    const speed = 100; // 文字が入力される速度 (ms)
+    const speed = 200; // 文字が入力される速度 (ms)
 
-    const type = () => {
-      if (indexRef.current < text.length - 1) {
-        setDisplayText((prev) => prev + text[indexRef.current]);
-        indexRef.current++; // useRef なので確実に更新される
-        setTimeout(type, speed); // 再帰的に setTimeout を呼び出す
-      }
-    };
+    if (index < text.length) {
+      const timer = setTimeout(() => {
+        setDisplayText((prev) => prev + text[index]);
+        setIndex(index + 1);
+        setShowCursor(true);
+      }, speed);
 
-    type(); // 初回呼び出し
-
-    return () => {
-      indexRef.current = text.length; // クリーンアップ
-    };
-  }, []);
+      return () => clearTimeout(timer);
+    } else {
+      setShowCursor(false);
+    }
+  }, [index]);
 
   return (
     <div className={styles.entranceContainer}>
-      <div className={styles.entranceText}>
-        {displayText.split("\n").map((line, index) => (
-          <React.Fragment key={index}>
-            {line}
-            <br />
-          </React.Fragment>
-        ))}
-      </div>
+      <div
+        className={styles.entranceText}
+        dangerouslySetInnerHTML={{
+          __html: displayText.replace(/\n/g, "<br />") + (showCursor ? '<span class="cursor">|</span>' : ""),
+        }}
+      />
+      <button type="button" className={styles.entranceBtn}>
+        <span className={styles.entranceBtn__text}>寄り道する</span>
+        <span className={styles.entranceBtn__mark}>＞＞＞</span>
+      </button>
       <Ajisai />
       <Ripple />
       <Pool />
